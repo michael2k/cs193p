912f17740429fede2399b1baca9e82185d8c5289
michael2k <mskim2k@gmail.com>
2021-06-27 18:06:02 +0900

    applied access controls

--- 2020/Memorize/Memorize/EmojiMemoryGame.swift
+++ 2020/Memorize/Memorize/EmojiMemoryGame.swift
@@ -11,7 +11,7 @@ class EmojiMemoryGame: ObservableObject {
     
     @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
-    static func createMemoryGame() -> MemoryGame<String> {
+    private static func createMemoryGame() -> MemoryGame<String> {
         let emojis = ["👻","🎃", "🕷"]
         return MemoryGame<String>(numberOfPariOfCards: emojis.count) { pairIndex in
             return emojis[pairIndex]
