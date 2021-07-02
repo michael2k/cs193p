a463595b25225109326b5bf902b96c216a570b85
michael2k <mskim2k@gmail.com>
2021-07-02 23:55:50 +0900

    lecture 6 animation

--- 2020/Memorize/Memorize/Cardify.swift
+++ 2020/Memorize/Memorize/Cardify.swift
@@ -7,19 +7,37 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
-    var isFaceUp: Bool
+struct Cardify: AnimatableModifier {
+    var rotation: Double
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var isFaceUp: Bool {
+        rotation < 90
+    }
+    
+    var animatableData: Double {
+        get { return rotation }
+        set { rotation = newValue }
+    }
     
     func body(content: Content) -> some View {
         ZStack {
-            if isFaceUp {
+            Group {
                 RoundedRectangle(cornerRadius: conrnerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: conrnerRadius).stroke(lineWidth: edgeLineWidth)
                 content
-            } else {
-                RoundedRectangle(cornerRadius: conrnerRadius).fill()
             }
+                .opacity(isFaceUp ? 1 : 0)
+            RoundedRectangle(cornerRadius: conrnerRadius).fill()
+                .opacity(isFaceUp ? 0 : 1)
         }
+        .rotation3DEffect(
+            Angle.degrees(rotation),
+            axis: (0,1,0)
+        )
     }
     
     private let conrnerRadius: CGFloat = 10.0
--- 2020/Memorize/Memorize/EmojiMemoryGameView.swift
+++ 2020/Memorize/Memorize/EmojiMemoryGameView.swift
@@ -14,14 +14,16 @@ struct EmojiMemoryGameView: View {
         VStack {
         Grid(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
-                    self.viewModel.choose(card: card)
+                    withAnimation(.linear(duration: 0.75)) {
+                        self.viewModel.choose(card: card)
+                    }
                 }
                 .padding(5)
             }
                 .padding()
         .foregroundColor(Color.orange)
             Button(action: {
-                withAnimation(.easeInOut(duration: 2)) {
+                withAnimation(.easeInOut) {
                     self.viewModel.resetGame()
                 }
             }, label: {
@@ -42,25 +44,46 @@ struct CardView: View {
         })
     }
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusRemaining * 10)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
-                    .padding(5).opacity(0.3)
+                Group {
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees( -animatedBonusRemaining*360-90), clockwise: true)
+                            .onAppear {
+                                self.startBonusTimeAnimation()
+                            }
+                    } else {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees( -card.bonusRemaining*360-90), clockwise: true)
+                    }
+                    
+                }
+                .padding(5).opacity(0.3)
+                .transition(.identity)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
                     .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
-                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
+            .transition(AnyTransition.scale)
         }
     }
     
     // MARK: - Drawing Contents
     
     private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 
