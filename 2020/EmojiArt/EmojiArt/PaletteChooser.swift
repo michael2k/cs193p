7b0bc892261aeda3829c1a79a1e0a5bf9c5aa06c
michael2k <mskim2k@gmail.com>
2021-07-08 22:49:08 +0900

    added palette chooser

---
+++ 2020/EmojiArt/EmojiArt/PaletteChooser.swift
@@ -0,0 +1,20 @@
+//
+//  PaletteChooser.swift
+//  EmojiArt
+//
+//  Created by Michael M. Kim on 2021/07/08.
+//
+
+import SwiftUI
+
+struct PaletteChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaletteChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteChooser()
+    }
+}
