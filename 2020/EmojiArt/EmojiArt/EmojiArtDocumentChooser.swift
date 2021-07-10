1788ed087aa00c404ca7cddf6df46bc886506714
michael2k <mskim2k@gmail.com>
2021-07-10 16:42:56 +0900

    added EmojiArtDocumentChooser

---
+++ 2020/EmojiArt/EmojiArt/EmojiArtDocumentChooser.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiArtDocumentChooser.swift
+//  EmojiArt
+//
+//  Created by Michael M. Kim on 2021/07/10.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiArtDocumentChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiArtDocumentChooser()
+    }
+}
