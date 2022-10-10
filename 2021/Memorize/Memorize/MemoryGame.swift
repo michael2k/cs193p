8f814881063fe1872d2154772696d1a4fb365cf8
michael2k <mskim2k@gmail.com>
2022-10-10 23:36:39 +0900

    ended L4

--- 2021/Memorize/Memorize/MemoryGame.swift
+++ 2021/Memorize/Memorize/MemoryGame.swift
@@ -7,15 +7,29 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
+    
+    private var indexOfTheOneAndOnlyFacUpCard: Int?
     
     mutating func choose(_ card: Card) {
         // if let chosenIndex = index(of: card) {
         // if let chosenIndex = cards.firstIndex(where: { aCardINTheCardsArray in aCardINTheCardsArray.id == card.id }){
-        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }){
-            
-            
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id })
+            , !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched
+        {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFacUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                indexOfTheOneAndOnlyFacUpCard = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfTheOneAndOnlyFacUpCard = chosenIndex
+            }
             cards[chosenIndex].isFaceUp.toggle()
         }
         print("\(cards)")
@@ -41,7 +55,7 @@ struct MemoryGame<CardContent> {
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
         var id: Int
